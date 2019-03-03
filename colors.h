35fe34e2d7e361c70b17c4ad5f0303ecf3c175f5
Gamal Othman <eng.gamalothman@gmail.com>
2019-03-03 17:47:28 +0200

    added files

---
+++ colors.h
@@ -0,0 +1,19 @@
+#ifndef _colors_
+#define _colors_
+
+// normal colors:
+#define RED 		"\x1B[0;31m"
+#define GREEN 		"\x1B[0;32m"
+#define YELLOW		"\x1B[0;33m"
+#define BLUE		"\x1B[0;34m"
+#define WHITE 		"\x1B[0;37m"
+#define RESET 		"\x1B[0m"
+
+// bright colors:
+#define BRI_RED		"\x1B[1;31m"
+#define BRI_GREEN	"\x1B[1;32m"
+#define BRI_YELLOW	"\x1B[1;33m"
+#define BRI_BLUE	"\x1B[1;34m"
+#define BRI_WHITE	"\x1B[1;37m"
+
+#endif
